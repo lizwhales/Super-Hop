9bc06d93738cd2402fb11e7a9de9bc1f00f80f52
Caitlin <37007242+cairinmisa@users.noreply.github.com>
2022-09-12 22:35:04 +1000

    Update SubmissionInfo.cs
    Completing half of the SubmissionInfo required for the Group Game Plan

--- Assets/SubmissionInfo.cs
+++ Assets/SubmissionInfo.cs
@@ -9,25 +9,24 @@ public static class SubmissionInfo
     // for extra details related to use of this file.
 
     // URL to your group's project 2 repository on GitHub.
-    public static readonly string RepoURL = "https://github.com/COMP30019/project-2-";
+    public static readonly string RepoURL = "https://github.com/COMP30019/project-2-team-ok_hand";
     
     // Come up with a team name below (plain text, no more than 50 chars).
-    public static readonly string TeamName = "Replace with your team name";
+    public static readonly string TeamName = "Team :ok_hand:";
     
     // List every team member below. Ensure student names/emails match official
     // UniMelb records exactly (e.g. avoid nicknames or aliases).
     public static readonly TeamMember[] Team = new[]
     {
-        new TeamMember("Student Name 1", "...@student.unimelb.edu.au"),
-        new TeamMember("Student Name 2", "...@student.unimelb.edu.au"),
-        new TeamMember("Student Name 3", "...@student.unimelb.edu.au"),
-        // Remove the following line if you have a group of 3
-        new TeamMember("Student Name 4", "...@student.unimelb.edu.au"), 
+        new TeamMember("Caitlin Grant", "caitling1@student.unimelb.edu.au"),
+        new TeamMember("Elizabeth Wong", "wonges@student.unimelb.edu.au"),
+        new TeamMember("Benjamin Yi", "benjaminchen@student.unimelb.edu.au"),
+        new TeamMember("Sean Maher", "spmaher@student.unimelb.edu.au"), 
     };
 
     // This may be a "working title" to begin with, but ensure it is final by
     // the video milestone deadline (plain text, no more than 50 chars).
-    public static readonly string GameName = "Replace with your game name";
+    public static readonly string GameName = "Paving The Way";
 
     // Write a brief blurb of your game, no more than 200 words. Again, ensure
     // this is final by the video milestone deadline.
@@ -35,6 +34,7 @@ public static class SubmissionInfo
 @"Write a blurb of your game here in plain text. This is a
 multiline verbatim string,
 so don't worry about keeping it to one line, just be sure not to indent it.
+// TO BE DONE BEFORE FRIDAY //
 ";
     
     // By the gameplay video milestone deadline this should be a direct link
