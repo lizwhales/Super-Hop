6c16619c8abc83a7fe48b1ec0331db896c13358b
xSPM <spmaher@student.unimelb.edu.au>
2022-09-20 22:06:29 +1000

    Added current movement mechanics
    Added the movement mechanics from the player movement branch to test levels, in the process, merging some of the formatting

---
+++ Assets/Scripts/Movement/PlayerCamera.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float xSense;
+    public float ySense;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start(){
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update(){
+        // get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * xSense;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * ySense;
+
+        // to handle the rotations in unity 
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        // clamp to restrict looking up and down more than 90 deg
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+
+    }
+
+}
+
---
+++ Assets/Scripts/Movement/WallJumping.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallJumping : MonoBehaviour
+{
+
+    [Header("Wall Jumping")]
+    public LayerMask Wall;
+    public LayerMask Ground;
+
+    float wallJumpUpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void WallJump(){
+        
+    }
+}
