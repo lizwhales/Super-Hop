ad228b72dfa7d9efae7345c1e289271fded1122d
Elizabeth Wong <elizabeth.wong@student.unimelb.edu.au>
2022-09-20 17:44:44 +1000

    change cursor visibility to false

--- Assets/Scripts/Movement/PlayerCamera.cs
+++ Assets/Scripts/Movement/PlayerCamera.cs
@@ -14,7 +14,7 @@ public class PlayerCamera : MonoBehaviour
 
     private void Start(){
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = true;
+        Cursor.visible = false;
     }
 
     private void Update(){
