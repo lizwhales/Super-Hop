0c06a6f14bbb11aba3e8c5a7e96b2a0a2a15f673
byi649 <byi649@aucklanduni.ac.nz>
2022-09-23 03:24:11 +1000

    Build hotkeys

--- Assets/Scripts/Object Placer/CubePreview.cs
+++ Assets/Scripts/Object Placer/CubePreview.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class CubePreview : MonoBehaviour
 {
 
+    bool instantiated = false;
     RaycastHit hit;
-    Vector3 movePoint;
-    public GameObject prefab;
+    public GameObject PreviewPlayerCube;
+    public GameObject PlayerCube;
+    GameObject previewPlayerCubeInstance;
+    GameObject playerCubeInstance;
     Ray ray;
     Vector3 centerPointer = new Vector3(0.5F, 0.5F, 0);
     float maxBuildDistance = 100.0F;
     Vector3 cubeOffsets = new Vector3(2F, 0.5F, 2F);
+    public KeyCode buildCubeKey = KeyCode.Alpha1;
+    public KeyCode confirmBuild = KeyCode.Mouse0;
+    public KeyCode cancelBuild = KeyCode.Mouse1;
 
     // Start is called before the first frame update
     void Start()
@@ -22,15 +28,30 @@ public class CubePreview : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ray = Camera.main.ViewportPointToRay(centerPointer);
-        if (Physics.Raycast(ray, out hit, maxBuildDistance, LayerMask.GetMask("Ground"))) {
-            transform.position = hit.point + Vector3.Scale(hit.normal, cubeOffsets);
+
+        if (Input.GetKeyDown(buildCubeKey) && instantiated == false) {
+            previewPlayerCubeInstance = Instantiate(PreviewPlayerCube);
+            instantiated = true;
         }
 
-        if (Input.GetMouseButtonDown(0)) {
-            Instantiate(prefab, transform.position, transform.rotation);
-            Destroy(gameObject);
+        if (instantiated == true) {
+            ray = Camera.main.ViewportPointToRay(centerPointer);
+            if (Physics.Raycast(ray, out hit, maxBuildDistance, LayerMask.GetMask("Ground"))) {
+                previewPlayerCubeInstance.transform.position = hit.point + Vector3.Scale(hit.normal, cubeOffsets);
+            }
+
+            if (Input.GetKeyDown(confirmBuild)) {
+                playerCubeInstance = Instantiate(PlayerCube, previewPlayerCubeInstance.transform.position, previewPlayerCubeInstance.transform.rotation);
+                Destroy(previewPlayerCubeInstance);
+                instantiated = false;
+            }
+
+            if (Input.GetKeyDown(cancelBuild)) {
+                Destroy(previewPlayerCubeInstance);
+                instantiated = false;
+            }
+
         }
-        
+
     }
 }
