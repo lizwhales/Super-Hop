73de37d1c42b8ef17eae221bbe0a8c8e9d73adbe
xSPM <110758187+xSPM@users.noreply.github.com>
2022-09-28 18:32:48 +1000

    Implemented level generation for vertical aspect
    Added level generation for multiple layers in the y axis, also added wall generation, all of this via a txt file. The format of the text file has been modified to implement this see test.txt for new format and test1.txt for old format

--- Assets/Scripts/Level Generation/LevelGenerator.cs
+++ Assets/Scripts/Level Generation/LevelGenerator.cs
@@ -6,10 +6,34 @@ using System.Text.RegularExpressions;
 
 
 public class LevelGenerator : MonoBehaviour
-{
+{   
+    // declaring constants
+
+    // level constants
+    public const int LAYER_SPACING = 2;
+    private const int LEVEL_WIDTH = 5;
+
+    // tile constants
+    public const int TILE_WIDTH = 4;
+    public const int TILE_HEIGHT = 1;
+
+    // wall constants
+    public const int WALL_LENGTH = 4;
+    public const int WALL_HEIGHT = 2;
+    public const float LEFT_WALL_X_OFFSET = -2.5f;
+    public const float RIGHT_WALL_X_OFFSET = LEVEL_WIDTH * TILE_WIDTH - 1.5f;
+    public const float WALL_Y_OFFSET = -0.5f;
+    
+
+
+    
+
+
+
     // declaring transformations for a given object
    
     public Transform cube;
+    public Transform wall;
     public Transform player;
     public Transform start;
     public Transform goal;
@@ -18,41 +42,93 @@ public class LevelGenerator : MonoBehaviour
     public const string sVoid = "-";
     public const string sTile = "x";
     public const string sGoal = "G";
+    public const string sWall = ">";
 
 
-    string[][] readFile(string file){
+    public const int numLayers = 6;
+    // layer 0 to 5 :
+    // layer 0 -> y = 0 level
+    // layer 1 -> y = 1 level
+    // layer 2 -> y = 2 level
+    // layer 3 -> y = 3 level
+    // layer 4 -> left wall
+    // layer 5 -> right wall
+    string[][] readLayer(int layer, string file){
         string text = System.IO.File.ReadAllText(file);
-        string[] lines = Regex.Split(text, "\r\n");
+        string [] layers = Regex.Split(text, "#");
+        Debug.Log("number of layers: "+ layers.Length);
+        string curLayer = layers[layer];
+        string[] lines = Regex.Split(curLayer, ",");
+        Debug.Log("This is layer: " + layer);
+        for (int k = 0; k < lines.Length; k++){
+            Debug.Log(lines[k]);
+        }
         int rows = lines.Length;
-        
-        string[][] levelBase = new string[rows][];
+    
+        string[][] layerBase = new string[rows][];
         for (int i = 0; i < lines.Length; i++)  {
             string[] stringsOfLine = Regex.Split(lines[i], " ");
-            levelBase[i] = stringsOfLine;
+            layerBase[i] = stringsOfLine;
         }
-        return levelBase;
+        return layerBase;
+    }
+    void generateLayer (int layer, string file){
+        string[][] curLayer = readLayer(layer, file);
+        if (layer < 4) {
+            // create a layer based on the text file
+            for (int z = 0; z < curLayer.Length - 1; z++) { // current layers length - 1 to remove empty field between layers
+                for (int x = 0; x < curLayer[0].Length; x++) {
+                    switch (curLayer[z][x]){
+                    case sTile:
+                        Instantiate(cube, new Vector3(x * TILE_WIDTH, layer * LAYER_SPACING, z * TILE_WIDTH), Quaternion.identity);
+                        break;
+                    case sGoal:
+                        Instantiate(goal, new Vector3(x * TILE_WIDTH, layer * LAYER_SPACING, z * TILE_WIDTH), Quaternion.identity);
+                        break;
+                    case sVoid:
+                        break; 
+                    } 
+                
+                }        
+            }
+        } else {
+            // create left wall based on the text file
+            if (layer == 4){
+                for (int z = 0; z < curLayer.Length - 1; z++) { 
+                    for (int y = 0; y < curLayer[0].Length; y++) {
+                        switch (curLayer[z][y]){
+                        case sWall:
+                            Instantiate(wall, new Vector3(LEFT_WALL_X_OFFSET, y * WALL_HEIGHT + WALL_Y_OFFSET, z*WALL_LENGTH), Quaternion.identity);
+                            break;
+                        case sVoid:
+                            break; 
+                        } 
+                    }        
+                }
+            // create right wall based on the text file
+            } else if (layer == 5) {
+                for (int z = 0; z < curLayer.Length - 1; z++) { 
+                    for (int y = 0; y < curLayer[0].Length; y++) {
+                        switch (curLayer[z][y]){
+                        case sWall:
+                            Instantiate(wall, new Vector3(RIGHT_WALL_X_OFFSET, y * WALL_HEIGHT + WALL_Y_OFFSET, z * WALL_LENGTH), Quaternion.identity);
+                            break;
+                        case sVoid:
+                            break; 
+                        } 
+                    
+                    }        
+                }
+            }
+            
+        }
     }
     // Start is called before the first frame update
     void Start()
     {
-        string[][] level = readFile("Assets/test.txt");
-        Debug.Log(level);
-     
-        // create a level based on the text file
-        for (int z = 0; z < level.Length; z++) {
-            for (int x = 0; x < level[0].Length; x++) {
-                switch (level[z][x]){
-                case sTile:
-                    Instantiate(cube, new Vector3(x*4, 0, z*4), Quaternion.identity);
-                    break;
-                case sGoal:
-                    Instantiate(goal, new Vector3(x, 0, z), Quaternion.identity);
-                    break;
-                case sVoid:
-                    break; 
-                } 
-            
-            }        
+        int i = 0;
+        for (i = 0; i < numLayers; i++){
+            generateLayer(i, "Assets/test.txt");
         }
     }
     
