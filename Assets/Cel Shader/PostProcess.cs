e3388d015518a1ed3c8f703ce9210918399705c2
Caitlin <37007242+cairinmisa@users.noreply.github.com>
2022-10-06 00:12:38 +1100

    Basic Post Processing Script
    Added in the script, attached to main camera.

--- Assets/Cel Shader/PostProcess.cs
+++ Assets/Cel Shader/PostProcess.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class PostProcess : MonoBehaviour
 {
-    void OnRenderImage(RenderTexture src, RenderTexture dst) {
-        Debug.Log("Hemlo!");
-        Graphics.Blit(src, dst);
+    [SerializeField] Shader bloomShader;
+    [SerializeField] Material bloom;
+    [Range(0, 10)] public float threshold = 1;
 
+    void OnRenderImage(RenderTexture src, RenderTexture dst) {
+        // int width = src.width / 4;
+        // int height = src.height / 4;
+        // RenderTextureFormat format = src.format;
+
+        // RenderTexture temp = RenderTexture.GetTemporary(src.width, src.height,0, src.format);
+        // Graphics.Blit(src, temp);
+        // Graphics.Blit(temp, dst);
+        // RenderTexture.ReleaseTemporary(temp);
+        
+        Graphics.Blit(src, dst);
     }
 }
