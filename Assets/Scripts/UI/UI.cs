5f43b0979cd73f164c75292c0187029b5e9d22aa
Caitlin <37007242+cairinmisa@users.noreply.github.com>
2022-10-24 15:05:53 +1100

    Fix Pause Menu, add in tutorial text, change music

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -7,18 +7,63 @@ public class UI : MonoBehaviour
 {
 
     public static bool PausedGame = false;
-    [SerializeField] public GameObject _pauseMenu;
-    [SerializeField] public GameObject _crosshair;
-    [SerializeField] public GameObject _winPanel;
-    [SerializeField] public GameObject _losePanel;
-    [SerializeField] public GameObject _objPanel;
+    public GameObject _pauseMenu;
+    public GameObject _crosshair;
+    public GameObject _winPanel;
+    public GameObject _losePanel;
+    public GameObject _objPanel;
+
+    public GameObject level1Text;
+    public GameObject level2Text;
+    public GameObject level3Text;
+
+    GameObject gameScripts;
+    string levelIdentifier;    
+
+
     // Start is called before the first frame update
     void Start()
     {
+        // Set up Cursor and Time
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0f;
+        
+        // Set up UI panel
+        _objPanel.SetActive(true);
+        _crosshair.SetActive(false);
+
+        // Set up Tutorial Text
+        gameScripts = GameObject.Find("GameScripts");
+        levelIdentifier = gameScripts.GetComponent<LevelGenerator>().levelFile;
+
+        if (levelIdentifier == "Assets/Levels/level_1.txt")
+        {
+            level3Text.SetActive(false);
+            level2Text.SetActive(false);
+            level1Text.SetActive(true);
+        }
+        
+        if (levelIdentifier == "Assets/Levels/level_2.txt")
+        {
+            level3Text.SetActive(false);
+            level1Text.SetActive(false);
+            level2Text.SetActive(true);
+        }
+
+        if (levelIdentifier == "Assets/Levels/ice_level.txt")
+        {
+            level1Text.SetActive(false);
+            level2Text.SetActive(false);
+            level3Text.SetActive(true);
+        }
+    }
+
+    public void RemoveTutText(){
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         Time.timeScale = 1f;
-        _objPanel.SetActive(true);
+        _crosshair.SetActive(true);
     }
 
     // Update is called once per frame
@@ -45,15 +90,33 @@ public class UI : MonoBehaviour
     public void ResumeGame(){
         Time.timeScale = 1f;
         PausedGame = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         _pauseMenu.SetActive(false);
         _crosshair.SetActive(true);
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-        
+        ManagePauseMenu();
+    }
+
+    public void ManagePauseMenu(){
+        GameObject pauseMainMenu = _pauseMenu.transform.GetChild(0).GetChild(0).gameObject;
+        GameObject optionsMenu = _pauseMenu.transform.GetChild(0).GetChild(1).gameObject;
+
+        if(optionsMenu.activeSelf == true)
+        {
+            optionsMenu.SetActive(false);
+            pauseMainMenu.SetActive(true);
+        }
     }
 
     public void MainMenu(){
         _pauseMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0f;
+        PausedGame = false;
+        _objPanel.SetActive(false);
+        _crosshair.SetActive(false);
+        ManagePauseMenu();
         SceneManager.LoadScene(0);
     }
 
