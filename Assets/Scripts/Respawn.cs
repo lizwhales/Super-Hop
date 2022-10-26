ba1fdce857bb85b380f7dcc9c29f058ffb150b8f
xSPM <110758187+xSPM@users.noreply.github.com>
2022-10-26 22:43:43 +1100

    adjusting kill box
    kill box now much smaller and follows the player as they move through the level

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -6,9 +6,21 @@ public class Respawn : MonoBehaviour
 {
     [SerializeField] public GameObject player;
     [SerializeField] public Transform spawnPoint;
+    private GameObject kb;
+    private Vector3 kbPos;
+
+    
     
     public int spikePenalty = 3;
-
+    private float z;
+    void Start(){
+        kb = GameObject.Find("KillBox");
+        kbPos = kb.transform.position;
+    }
+    void Update() {
+        z = player.transform.position.z;
+        kb.transform.position = new Vector3(kbPos.x, kbPos.y, z);
+    }
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "DeathTrigger")
